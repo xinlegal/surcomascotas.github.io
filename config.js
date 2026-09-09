a0be6659278725c6379b1d4e2bc067e91cbed8dc
xinlegal <prensaperuana2021@gmail.com>
2026-09-09 16:01:38 -0500

    Configurar WhatsApp del administrador

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Reemplaza estos datos antes de publicar la web.
 
 window.SURCO_MASCOTAS_CONFIG = {
-  whatsappAdmin: "51999999999",
+  whatsappAdmin: "51963762075",
   facebookUrl: "https://web.facebook.com/profile.php?id=100070698631769"
 };
