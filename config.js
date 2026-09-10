9bcc8b48dbd855cef5355c32e9e775c7f365bd2f
xinlegal <prensaperuana2021@gmail.com>
2026-09-10 10:20:13 -0500

    Conectar Surco Mascotas con Supabase

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // CONFIGURACIÓN BÁSICA
-// Reemplaza estos datos antes de publicar la web.
-
 window.SURCO_MASCOTAS_CONFIG = {
   whatsappAdmin: "51963762075",
-  facebookUrl: "https://web.facebook.com/profile.php?id=100070698631769"
+  facebookUrl: "https://web.facebook.com/profile.php?id=100070698631769",
+  supabaseUrl: "https://xhtcijcsledoaqqichoq.supabase.co",
+  supabaseKey: "sb_publishable_44lpFdek1noeWAvNZlCqzA_YmH9xQjs"
 };
